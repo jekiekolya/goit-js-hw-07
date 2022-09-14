d3bf7f9e781e137b377666c2ab2b4cf028785d97
Mykola Berezhniuk <bermukola@gmail.com>
2022-09-14 19:27:37 +0300

    1EX

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this lins);
 
-console.log(galleryItems);
+// Getting Ref
+const galleryBox = document.querySelector('.gallery');
+
+// Create gallery markup
+let galleryMarkupString = '';
+function createGalleryMarkup(array) {
+  for (const img of array) {
+    galleryMarkupString += `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+      
+    />
+  </a>
+</div>`;
+  }
+
+  return galleryMarkupString;
+}
+
+// Add gallery MarkUp on web page
+galleryBox.innerHTML = createGalleryMarkup(galleryItems);
+
+//Add event listener and function imgClickTargetHandler
+galleryBox.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  let linkToOriginPhoto = event.target.dataset.source;
+
+  // Create modal window
+  const instance = basicLightbox.create(`
+    <img src="${linkToOriginPhoto}" width="800" height="600">
+`);
+  // Show modal window
+  instance.show();
+
+  // Close modal window after pressed ESC
+  window.addEventListener('keydown', pressKeyTargetHandler);
+  function pressKeyTargetHandler(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', pressKeyTargetHandler);
+    }
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Get Ref
+const list = document.querySelector('.gallery');
+
+// Create gallery markup
+let galleryMarkupString = '';
+function createGalleryMarkup(gallery) {
+  for (const img of gallery) {
+    galleryMarkupString += `<li>
+  <a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="Image description" />
+  </a>
+</li>`;
+  }
+  return galleryMarkupString;
+}
+list.innerHTML = createGalleryMarkup(galleryItems);
+
+// Add event listener
+list.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  var lightbox = $('.gallery a').simpleLightbox({
+    /* options */
+  });
+
+  lightbox.open();
+}
